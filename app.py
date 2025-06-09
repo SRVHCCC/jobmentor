fb1c76f6e88ff7fc7ca0ad07bfa6edd0fc902ca7
HImanshu Chaurasiya <125813224+SRVHCCC@users.noreply.github.com>
2025-06-09 13:53:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -420,8 +420,9 @@ def ats_scores():
 
 
 
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
